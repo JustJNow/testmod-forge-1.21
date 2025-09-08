ce770a18926b2f01a198cf0b5c7f2d336928dd2b
JustJNow <joppe.bergmans@gmail.com>
2025-09-08 11:24:04 +0200

    Added chisel from Kaupenjoe tutorial

--- src/main/java/net/justjnow/testmod/item/ModItems.java
+++ src/main/java/net/justjnow/testmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.justjnow.testmod.item;
 
 import net.justjnow.testmod.TestMod;
+import net.justjnow.testmod.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,6 +17,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
             ()-> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel",
+            ()-> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
